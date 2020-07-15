9805413b92a2c003b0d549c7cccc9b2b34316863
oura1112 <59088115+oura1112@users.noreply.github.com>
2020-07-15 20:59:27 +0900

    0715

--- programs/wealth_process.py
+++ programs/wealth_process.py
@@ -7,6 +7,7 @@ Created on Sun Jul 12 16:17:14 2020
 
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 class optimal_wealth_process():
     
@@ -15,7 +16,7 @@ class optimal_wealth_process():
         self.r = 0.01
         self.u = 0.2
         self.l = -0.2
-        self.T = 100
+        self.T = 60000
         self.R_bar = self.p * self.u + (1 - self.p) * self.l # = 0.02
         self.X_0 = X_0
         self.X_t = self.X_0
@@ -40,13 +41,37 @@ class optimal_wealth_process():
             X_t_process.append(self.X_t)
             X_t_mean_process.append(self.X_t_mean)
             
-        plt.plot(time, X_t_process, label="wealth")
-        plt.plot(time, X_t_mean_process, label="mean of wealth")
+        #plt.plot(time, X_t_process, label="wealth")
+        #plt.plot(time, X_t_mean_process, label="mean of wealth")
         plt.legend(loc='upper left')
         plt.xlabel("time")
         plt.ylabel("wealth")
-        plt.show()        
+        plt.show()
+        
+    def log_process(self):
+        
+        time = []
+        log_process = []
+        
+        for t in range(self.T):
+            if np.random.uniform(0,1) < self.p:
+                R_t = self.u
+            else:
+                R_t = self.l
+                
+            self.X_t *= ( 1 + (self.R_bar - self.r)*(R_t - self.r) / ( (self.u - self.r)*(self.r - self.l) ) )
+            
+            time.append(t)
+            log_process.append(math.log(self.X_t)/(t+1))
+            
+        plt.plot(time, log_process)
+        plt.legend(loc='upper left')
+        plt.xlabel("time")
+        plt.ylabel("log_wealth_per_time")
+        plt.show()
+        print(log_process[-1])
 
-optimal_wealth_process = optimal_wealth_process(X_0=10)
+optimal_wealth_process = optimal_wealth_process(X_0=1)
 
-optimal_wealth_process.optimal_wealth()
+#optimal_wealth_process.optimal_wealth()
+optimal_wealth_process.log_process()
